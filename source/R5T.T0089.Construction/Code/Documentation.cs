fb553c8ae628703d8b79cba977ec18b488d59258
davidcoats <david@getsafetycone.com>
2021-11-11 16:29:41 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0089.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0089.Construction
+{
+    /// <summary>
+    /// Construction project for IAnsiColorCode extension method base..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0089/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0089
+{
+    /// <summary>
+    /// IAnsiColorCode extension method base.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
